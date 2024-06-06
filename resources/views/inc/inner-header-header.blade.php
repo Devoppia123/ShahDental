3f65b792765184ecbb6d703e5cdb236a1dd315fa
Devoppia123 <assadullah@deimpex.com>
2024-06-06 15:46:41 +0500

    changes by Asad

--- resources/views/inc/inner-header-header.blade.php
+++ resources/views/inc/inner-header-header.blade.php
@@ -41,59 +41,7 @@ $url = $_SERVER['REQUEST_URI']; // Get the current URL
         default:
             echo 'callback';
             break;
-    }
-<<<<<<< Updated upstream
-@endphp" id="header-inner">
-  <div class="container-xxl">
-      <div class="header-txt-blk all-txt-blk">
-          <div class="top-header row">
-              <div class="empty-blk-01 col-md-2"></div>
-              <div class="col-md-12 col-sm-12 top-r">
-                  <div class="top-image-box call-us">
-                      <span>
-                          <img src="{{ url('public/images/Shah-Dental_03.png') }}" alt="03">
-                          : <a href="Gulshan-e-Iqbal Branch-021-34963440">
-                              <span class="call-bold"></span></a>
-                      </span>
-                  </div>
-                  <div class="top-txt-box address">
-                      <p>Gulshan-e-Iqbal Branch-<a href="tel:02134963440">021-34963440</a></p>
-                      <p>Bahadurabad-<a href="tel:03341326378">03341326378</a></p>
-                      <p>DHA Branch-<a href="tel:021-35243482">021-35243482</a></p>
-                  </div>
-              </div>
-          </div>
-          <div class="mian-nav">
-          <nav class="navbar navbar-expand-xl navbar-light">
-              <a class="navbar-brand" href="{{ url('/') }}">
-                  <img src="{{ url('public/images/logo_03.png') }}" alt="" class="d-inline-block align-text-top cus-logo">
-              </a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                  <ul class="navbar-nav">
-                      <li class="nav-item"><a class="nav-link" href="{{ url('/our-mission') }}">Our Mission</a></li>
-                      <li class="nav-item"><a class="nav-link" href="{{ url('/message') }}">Message</a></li>
-                      <li class="nav-item"><a class="nav-link" href="{{ url('/highlights') }}">Highlights</a></li>
-                      <li class="nav-item"><a class="nav-link" href="{{ url('/all-articles') }}">Articles</a></li>
-                      <li class="nav-item"><a class="nav-link" href="{{ url('/gallery') }}">Gallery</a></li>
-                      <li class="nav-item"><a class="nav-link" href="{{ url('/services-treatments') }}">Services/Treatments</a></li>
-                      <li class="nav-item"><a class="nav-link" href="{{ url('/contact-us') }}">Contact</a></li>
-                      <li class="nav-item"><a class="nav-link nav-btn-01" href="{{ url('/make-an-appointment') }}">MAKE AN APPOINTMENT</a></li>
-                  </ul>
-              </div>
-          </nav>
-      </div>
-          <div class="home-banner-content">
-          {{-- Display Success and Fail Status --}}
-          @if (session('success'))
-              <div class="alert alert-success">
-                  {{ session('success') }}
-              </div>
-          @endif
-=======
-        } @endphp"
+    }} @endphp"
     id="header-inner">
     <div class="container-xxl">
         <div class="header-txt-blk all-txt-blk">
@@ -108,113 +56,113 @@ $url = $_SERVER['REQUEST_URI']; // Get the current URL
                         </span>
                     </div>
                     <div class="top-txt-box address">
-                        <p>Gulshan-e-Iqbal Branch-021-34963440</p>
-                        <p>Bahadurabad-03341326378</p>
-                        <p>DHA Branch-021-35243482</p>
+                        <p>Gulshan-e-Iqbal Branch-<a href="tel:02134963440">021-34963440</a></p>
+                        <p>Bahadurabad-<a href="tel:03341326378">03341326378</a></p>
+                        <p>DHA Branch-<a href="tel:021-35243482">021-35243482</a></p>
                     </div>
                 </div>
             </div>
+            <div class="mian-nav">
+                <nav class="navbar navbar-expand-xl navbar-light">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        <img src="{{ url('public/images/logo_03.png') }}" alt=""
+                            class="d-inline-block align-text-top cus-logo">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item"><a class="nav-link" href="{{ url('/our-mission') }}">Our Mission</a>
+                            </li>
+                            <li class="nav-item"><a class="nav-link" href="{{ url('/message') }}">Message</a></li>
+                            <li class="nav-item"><a class="nav-link" href="{{ url('/highlights') }}">Highlights</a></li>
+                            <li class="nav-item"><a class="nav-link" href="{{ url('/all-articles') }}">Articles</a></li>
+                            <li class="nav-item"><a class="nav-link" href="{{ url('/gallery') }}">Gallery</a></li>
+                            <li class="nav-item"><a class="nav-link"
+                                    href="{{ url('/services-treatments') }}">Services/Treatments</a></li>
+                            <li class="nav-item"><a class="nav-link" href="{{ url('/contact-us') }}">Contact</a></li>
+                            <li class="nav-item"><a class="nav-link nav-btn-01"
+                                    href="{{ url('/make-an-appointment') }}">MAKE AN APPOINTMENT</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+            <div class="home-banner-content">
+                {{-- Display Success and Fail Status --}}
+                @if (session('success'))
+                    <div class="alert alert-success">
+                        {{ session('success') }}
+                    </div>
+                @endif
 
-            <nav class="navbar navbar-expand-xl navbar-light">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="{{ url('public/images/logo_03.png') }}" alt=""
-                        class="d-inline-block align-text-top cus-logo">
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item"><a class="nav-link" href="{{ url('/our-mission') }}">Our Mission</a></li>
-                        <li class="nav-item"><a class="nav-link" href="{{ url('/message') }}">Message</a></li>
-                        <li class="nav-item"><a class="nav-link" href="{{ url('/highlights') }}">Highlights</a></li>
-                        <li class="nav-item"><a class="nav-link" href="{{ url('/all-articles') }}">Articles</a></li>
-                        <li class="nav-item"><a class="nav-link" href="{{ url('/gallery') }}">Gallery</a></li>
-                        <li class="nav-item"><a class="nav-link"
-                                href="{{ url('/services-treatments') }}">Services/Treatments</a></li>
-                        <li class="nav-item"><a class="nav-link" href="{{ url('/contact-us') }}">Contact</a></li>
-                        <li class="nav-item"><a class="nav-link nav-btn-01"
-                                href="{{ url('/make-an-appointment') }}">MAKE AN APPOINTMENT</a></li>
-                    </ul>
-                </div>
-            </nav>
+                @if (session('error'))
+                    <div class="alert alert-danger">
+                        {{ session('error') }}
+                    </div>
+                @endif
 
-            {{-- Display Success and Fail Status --}}
-            @if (session('success'))
-                <div class="alert alert-success">
-                    {{ session('success') }}
-                </div>
-            @endif
->>>>>>> Stashed changes
-
-            @if (session('error'))
-                <div class="alert alert-danger">
-                    {{ session('error') }}
-                </div>
-            @endif
-
-            <div id="banner-block-01 row">
-                <div class="inner-heading">
-                    <h1>
-                        <strong>
-                            <?php
-                            $url = $_SERVER['REQUEST_URI']; // Get the current URL
-                            
-                            // Split the URL by "/" and get the last element
-                            $parts = explode('/', $url);
-                            $id = end($parts);
-                            
-                            // Check if the URL contains "services_details/" and display the title accordingly
-                            if (strpos($url, 'services_details/') !== false) {
-                                echo 'Service Details ';
-                            } elseif (strpos($url, 'view_article/') !== false) {
-                                echo 'View Articles ';
-                            } else {
-                                $path = request()->path();
-                                switch ($path) {
-                                    case 'our-mission':
-                                        echo 'Our Mission';
-                                        break;
-                                    case 'message':
-                                        echo 'Message';
-                                        break;
-                                    case 'highlights':
-                                        echo 'Highlights';
-                                        break;
-                                    case 'all-articles':
-                                        echo 'Articles';
-                                        break;
-                                    case 'gallery':
-                                        echo 'Gallery';
-                                        break;
-                                    case 'services-treatments':
-                                        echo 'Services/Treatments';
-                                        break;
-                                    case 'contact-us':
-                                        echo 'Contact Us';
-                                        break;
-                                    case 'make-an-appointment':
-                                        echo 'Make an Appointment';
-                                        break;
-                                    default:
-                                        echo 'Call Back';
-                                        break;
+                <div id="banner-block-01 row">
+                    <div class="inner-heading">
+                        <h1>
+                            <strong>
+                                <?php
+                                $url = $_SERVER['REQUEST_URI']; // Get the current URL
+                                
+                                // Split the URL by "/" and get the last element
+                                $parts = explode('/', $url);
+                                $id = end($parts);
+                                
+                                // Check if the URL contains "services_details/" and display the title accordingly
+                                if (strpos($url, 'services_details/') !== false) {
+                                    echo 'Service Details ';
+                                } elseif (strpos($url, 'view_article/') !== false) {
+                                    echo 'View Articles ';
+                                } else {
+                                    $path = request()->path();
+                                    switch ($path) {
+                                        case 'our-mission':
+                                            echo 'Our Mission';
+                                            break;
+                                        case 'message':
+                                            echo 'Message';
+                                            break;
+                                        case 'highlights':
+                                            echo 'Highlights';
+                                            break;
+                                        case 'all-articles':
+                                            echo 'Articles';
+                                            break;
+                                        case 'gallery':
+                                            echo 'Gallery';
+                                            break;
+                                        case 'services-treatments':
+                                            echo 'Services/Treatments';
+                                            break;
+                                        case 'contact-us':
+                                            echo 'Contact Us';
+                                            break;
+                                        case 'make-an-appointment':
+                                            echo 'Make an Appointment';
+                                            break;
+                                        default:
+                                            echo 'Call Back';
+                                            break;
+                                    }
                                 }
-                            }
-                            ?>
+                                ?>
 
-                        </strong>
-                    </h1>
+                            </strong>
+                        </h1>
+                    </div>
                 </div>
             </div>
         </div>
-<<<<<<< Updated upstream
-        </div>
-        
-      </div>
-  </div>
-=======
+
     </div>
->>>>>>> Stashed changes
+</div>
+
+</div>
+
 </div>
